5ee9321eb11d480004a39854cc1b60634216821f
bilal karahan <bilalkarahan309@gmail.com>
2022-09-03 17:08:51 +0300

    n.o.h.p

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int mat, fiz, kim, turk, tar, muz;
+        System.out.println("Mat not:");
+        mat=inp.nextInt();
+        System.out.println("Fizik not:");
+        fiz=inp.nextInt();
+        System.out.println("Kimya not:");
+        kim=inp.nextInt();
+        System.out.println("Türkçe not:");
+        turk=inp.nextInt();
+        System.out.println("Tarih not:");
+        tar=inp.nextInt();
+        System.out.println("Müzik not:");
+        muz=inp.nextInt();
+        double ORT= (mat+fiz+kim+turk+tar+muz)/6;
+        System.out.println( ORT>60 ? "\nsınıfı geçti" + ORT : "kaldı\n" + ORT);
+
+
+    }
+}
